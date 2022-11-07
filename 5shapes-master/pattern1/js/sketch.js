cb84b66ab510d1079f0c49f66c890781b02e4f7d
joelgoughbecker <116875565+joelgoughbecker@users.noreply.github.com>
2022-11-07 09:24:06 +0000

    Add files via upload

---
+++ 5shapes-master/pattern1/js/sketch.js
@@ -0,0 +1,60 @@
+//setup function runs once at start
+function setup() {
+    var canvas = createCanvas(800, 800);
+    canvas.parent("p5container");
+}
+//draw function is a continous loop, runs from the start,at 60fps
+function draw() {
+    background(220);
+    background('rgb(98,255,140)');
+  
+    circle(100, 100, 200);
+  fill('red');
+  
+  circle(300, 100, 200);
+  fill('red');
+    
+    circle(500, 100, 200);
+  fill('red');
+    
+    circle(700, 100, 200);
+  fill('red');
+    
+    
+    circle(100, 300, 200);
+  fill('red');
+    
+    circle(300, 300, 200);
+  fill('red');
+    
+    circle(500, 300, 200);
+  fill('red');
+    
+    circle(700, 300, 200);
+  fill('red');
+    
+    circle(100, 500, 200);
+  fill('red');
+    
+      circle(300, 500, 200);
+  fill('red');
+    
+      circle(500, 500, 200);
+  fill('red');
+    noStroke();
+      circle(700, 500, 200);
+  fill('red');
+    
+     circle(100, 700, 200);
+  fill('red');
+    
+    circle(300, 700, 200);
+  fill('red');
+    
+    circle(500, 700, 200);
+  fill('red');
+    
+    circle(700, 700, 200);
+  fill('red');
+  }
+
---
+++ 5shapes-master/pattern3/js/sketch.js
@@ -0,0 +1,47 @@
+//setup function runs once at start
+function setup() {
+    var canvas = createCanvas(800, 800);
+    canvas.parent("p5container");
+}
+//draw function is a continous loop, runs from the start,at 60fps
+function draw() {
+  background(220);
+  background('rgb(0,175,25)');
+  
+  line(800, 0, 0, 675);
+
+  strokeWeight(40)
+   color(255, 204, 0)
+   
+    line(600, 0, 0, 475);
+  
+      line(400, 0, 0, 275);
+  
+  
+
+    
+  line(1000, 0, 0, 875);
+  
+    
+  line(1200, 0, 0, 1075);
+   
+    
+  line(1400, 0, 0, 1275);
+  
+   circle(160, 140, 120);
+  
+   circle(260, 240, 120);
+  
+  circle(360, 340, 120);
+  
+  circle(460, 440, 120);
+  
+  circle(560, 540, 120);
+
+circle(660, 640, 120);
+  
+
+  
+ ;
+  }
+
